adb53537cbff38c3e1c172b553c078d89a2dbd86
dheeraj0808 <dheerajsingh1939@gmail.com>
2025-08-19 23:30:00 +0530

    .

--- basics/index.js
+++ basics/index.js
@@ -13,10 +13,10 @@ calculator(4, 5);
 
 //creating a finction in which it will tells us when someone can vote 
 function eligiblecheck(age) {
-    if(age<1){
+    if (age < 1) {
         console.log("age is not valid !");
     }
-    else if (age >=18) {
+    else if (age >= 18) {
         console.log("you are eligible to vote");
 
     }
@@ -32,9 +32,9 @@ eligiblecheck(19);
 eligiblecheck(65);
 
 //function 3 - square of a number - when we will return inside the fuction then we have recieve that value in a variable 
-function square (x){
-let result = x*x;
-return result;
+function square(x) {
+    let result = x * x;
+    return result;
 }
 let value = square(4);
 console.log("square of the number is " + value);
@@ -47,14 +47,46 @@ console.log(chalk.red("Error Message!"));
 console.log(chalk.green("Success!"));
 
 // creating a function for check even or odd
-function EvenOdd(number){
-    let rem=number%2;
-    if(rem==0){
+function EvenOdd(number) {
+    let rem = number % 2;
+    if (rem == 0) {
         console.log("Given number is even");
     }
-    else{
+    else {
         console.log("number is Odd");
     }
 }
 EvenOdd(5);
 EvenOdd(8);
+
+// looks is what when you have to do some repeatedly again and again then we will use loops
+function Hello(i) {
+    console.log("welcome to the city" + i);
+}
+for (let i = 0; i < 10; i++) {
+    Hello(i)
+}
+
+//length calculate of arr and impliment into loops
+let arr = [23, 45, 67, 98, 123, 2, 3, 4, 6];
+let length = arr.length;
+console.log("Total length of the array is " + length);
+
+// access array element to via loop
+for (let i = 0; i < arr.length; i++) {
+    console.log("array element at " + i + " position is " + arr[i]);
+}
+
+//printing only even number from the array
+for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+        console.log(arr[i]);
+    }
+}
+
+// printing only odd number from the array
+for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] % 2 != 0) {
+        console.log(arr[i]);
+    }
+}
