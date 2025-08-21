8104c0827f7160b71713ed159b676718677b8900
dheeraj0808 <dheerajsingh1939@gmail.com>
2025-08-22 00:37:29 +0530

    basics covered

--- basics/index.js
+++ basics/index.js
@@ -90,3 +90,131 @@ for (let i = 0; i <= arr.length; i++) {
         console.log(arr[i]);
     }
 }
+
+// write a function that searches for an element in an array and return the index, and if the element is not present then return -1
+function searchElement(arra, x) {
+    for (let i = 0; i < arra.length; i++) {
+        if (arra[i] == x) {
+            return i;
+        }
+    }
+    return -1;
+}
+console.log("seach element index result");
+let arra = [1, 2, 3, 4, 5, 6, 6, 78];
+let result = searchElement(arra, 5);
+console.log(result);
+
+// write a function that returns the number of negative number in the array
+function countNegative(array2) {
+    let count = 0;
+    for (let i = 0; i < array2.length; i++) {
+        if (array2[i] < 0) {
+            count++;
+        }
+    }
+    return count;
+}
+let array2 = [1, 2, -3, -4, -65, -7, -8];
+let result2 = countNegative(array2);
+console.log("total negative number in array");
+console.log(result2);
+
+//find largest number in the array3
+
+function findLargest(array3) {
+    let largest = -Infinity;
+    for (let i = 0; i < array3.length; i++) {
+        if (array3[i] > largest) {
+            largest = array3[i];
+        }
+    }
+    return largest;
+}
+let array3 = [12, 3, 34, 45, 345, -6, 76, 78, 90, 32];
+let result3 = findLargest(array3);
+console.log("largest number in the array3");
+console.log(result3);
+
+//finding smallest number in the array 3
+function findSmallest(array3) {
+    let smallest = +Infinity;
+    for (let i = 0; i < array3.length; i++) {
+        if (array3[i] < smallest) {
+            smallest = array3[i];
+        }
+    }
+    return smallest;
+}
+let result4 = findSmallest(array3);
+console.log("smallest number in the array 3");
+console.log(result4);
+
+// finding 2nd largest number in a given array
+
+function findSecondLargest(arr5) {
+    //in case of empty array or 1 element in that case it will show infinity
+    // so to solve infity issue we will add a case
+    if (arr5.length < 2) {
+        return null;
+    }
+    let firstlargest = -Infinity;
+    let secondLargest = -Infinity;
+    for (let i = 0; i < arr5.length; i++) {
+        if (arr5[i] > firstlargest) {
+            secondLargest = firstlargest;
+            firstlargest = arr5[i];
+        }
+        //in case of largest is duplicate ie doubly presented then we have to add condtion 
+        //if the most largest is double presented then that case it will not enter this loop
+        else if (arr5[i] > secondLargest && arr5[i] != firstlargest) {
+            secondLargest = arr5[i];
+        }
+    }
+    return secondLargest;
+
+}
+//let arr5=[];
+// when array length is less than 2 then the 2nd largest will be null
+let arr5 = [21, 3, 34, 56, 167, 167, 78, 98];
+let result5 = findSecondLargest(arr5);
+console.log("2nd largest number is " + result5);
+
+//loops in loop
+for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+        console.log("i =" + i + "j=" + j);
+    }
+}
+// 
+for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < i; j++) {
+        console.log("i=" + i + "j=" + j);
+    }
+}
+//
+console.log("j<=i testing");
+for (let i = 0; i < 5; i++) {
+    for (let j = 0; j <= i; j++) {
+        console.log("i=" + i + "j=" + j);
+    }
+}
+//
+console.log("j>0 testing");
+for (let i = 0; i < 5; i++) {
+    for (let j = i; j < 0; j++) {
+        //j<0 condition will never brak so it will go into infinite loop
+        console.log(j);
+    }
+}
+for (let i = 0; i < 5; i++) {
+    for (let j = i; j > 0; j--) {
+        console.log(i, j);
+    }
+}
+//
+for (let i=5;i>0;i--){
+    for(let j=0;j<i;j++){
+        console.log(i,j);
+    }
+}
