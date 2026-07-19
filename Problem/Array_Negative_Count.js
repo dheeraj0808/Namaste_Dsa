6847d989bfbc331850153e47946645a5c8d5e05c
Dheeraj Singh <dheerajsingh1939@gmail.com>
2026-07-19 14:20:30 +0530

    fiding the indexes of the negative number in the array

--- Problem/Array_Negative_Count.js
+++ Problem/Array_Negative_Count.js
@@ -1,3 +1,4 @@
+// Function to count the number of negative numbers in an array
 function countNegatives(arr) {
   let count = 0;
   for (let i = 0, n = arr.length; i < n; i++) {
@@ -9,3 +10,19 @@ function countNegatives(arr) {
 // Example 
 const nums = [3, -1, 0, -7, 8, -2, -5];
 console.log(countNegatives(nums));
+
+//2. Function to get the indexes of negative numbers in an array    
+function negativeIndexes(arr) {
+  const indexes = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 0) {
+      indexes.push(i);
+    }
+  }
+  return indexes;
+}
+
+// Example
+const nums = [4, -1, 7, -2, 0, -5];
+console.log(negativeIndexes(nums)); // [1, 3, 5]
+
