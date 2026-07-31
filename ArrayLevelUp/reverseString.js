47c892981103637f9248308385268af334735ec8
dheeraj0808 <dheerajsingh1939@gmail.com>
2026-07-31 21:04:12 +0530

    Implement reverseString function to reverse an array in-place

--- ArrayLevelUp/reverseString.js
+++ ArrayLevelUp/reverseString.js
@@ -1,2 +1,16 @@
 // write a program to reverse the array of character 
+// but we have to do it by modifying the input array by in-place no need to change the orignal array
+//-> modify this array means we have to re-arrange this array , we donot have to use the extra space 
 
+function reverseString(arr){
+    let n= arr.length;
+    let halfLen= Math.floor(n/2);
+
+    for(let i=0;i<halfLen;i++){
+       let temp = arr[i];
+        arr[i]=arr[n-i-1];
+        arr[n-i-1]=temp;
+    }
+    return arr;
+}
+console.log(reverseString(['h','e','l','l','o']));
