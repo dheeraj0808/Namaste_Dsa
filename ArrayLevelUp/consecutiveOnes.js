7790457cbaee311d6be9ae95f71e1f08a9a92372
dheeraj0808 <dheerajsingh1939@gmail.com>
2026-08-01 11:21:13 +0530

    program  of consecutinve k and consecutive one's

---
+++ ArrayLevelUp/consecutiveOnes.js
@@ -0,0 +1,18 @@
+// write a function that will count the number of 1's and return the max counted 1's
+
+    function consecutiveOne(arr){
+        let current_sum =0;
+        let maxCount=0;
+
+        for(let i=0;i<arr.length;i++){
+            if(arr[i]==1){
+                current_sum++;
+            }
+            else {
+                maxCount=Math.max(current_sum,maxCount);
+                current_sum=0;
+            }
+        }
+        return Math.max(current_sum,maxCount);
+    }
+    console.log(consecutiveOne([1,1,,3,3,4,5,2,1,1,1,1,5,0,0,7,5,2,1,1,1,1,1]))
