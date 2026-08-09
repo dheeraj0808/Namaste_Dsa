f6e86d7db80f4f34fbe0d79666363e3d67413173
dheeraj0808 <dheerajsingh1939@gmail.com>
2026-08-09 15:36:55 +0530

    understand bubble sort

---
+++ SearchingSorting/bubble.js
@@ -0,0 +1,4 @@
+// write a program to sort the number through bubble sort
+//-> after the first iteration the biggest element is go at there place 
+// -> this is same happening and keeps going and after each iteration one iteration will decreasing
+// because as of at each step iteration will decreasing due to highest element will go at that place
