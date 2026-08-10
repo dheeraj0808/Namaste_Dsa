d7d4607fc5f3366a3361281c3a50243d911aeea3
dheeraj0808 <dheerajsingh1939@gmail.com>
2026-08-10 22:57:27 +0530

    Implement bubble sort algorithm and add selection sort algorithm template

--- SearchingSorting/bubble.js
+++ SearchingSorting/bubble.js
@@ -2,3 +2,30 @@
 //-> after the first iteration the biggest element is go at there place 
 // -> this is same happening and keeps going and after each iteration one iteration will decreasing
 // because as of at each step iteration will decreasing due to highest element will go at that place
+let arr= [2,14,6,18,9,11,3]
+function bubble(arr){
+    let n = arr.length;
+
+    let isSwapped = false;
+for (let i=0;i<arr.length-1;i++) {
+    for (let j=0;j<n-1-i;j++) {
+        if (arr[j]>arr[j+1]) {
+            let temp = arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp;
+            isSwapped=true;
+        }
+    }
+    if(!isSwapped)break;
+}
+return arr;
+}
+let result = bubble(arr);
+console.log(result);
+
+/*
+if in any iteration no sorting happend ie no swapping then we need to stop bubble sort
+because array is already sort
+ex [1,2,3,4,5,7,]
+ex [12,3,5,6,7,8,]-> stop after 1st iteration 
+*/
---
+++ SearchingSorting/selection.js
@@ -0,0 +1,18 @@
+//in selection sort we will find the smallest element and keep to put that places 
+// so it will take n-1 because at the end the last element we need not to search because that are 
+// already sorted
+
+for(let i=0;i<n-1;i++){
+   let min =i;
+
+   for(let j=i+1;j<n;j++){
+    if(arr[j]<arr[min]){
+        min=j;
+    }
+   }
+   let temp = arr[i];
+   arr[i]=arr[min];
+   arr[min]=temp;
+
+
+}
