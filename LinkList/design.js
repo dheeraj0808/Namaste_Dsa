02b4f2585a08d081b48190b2d8dfbafbf1e49b6a
dheeraj0808 <dheerajsingh1939@gmail.com>
2026-08-16 00:25:35 +0530

    Add initial implementation of linked list node structure

---
+++ LinkList/design.js
@@ -0,0 +1,10 @@
+//---> this is the classic example (707 Design linked list ) 
+
+// node : creating a node in linked list
+// [data | next]
+function node (value){
+    this.value = value;
+    this.next = null;
+}
+
+// linked list is representation of its head
