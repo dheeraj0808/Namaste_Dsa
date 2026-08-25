ddefa159fae6dc7e3aaf1bc1e9abe7538615c173
Dheeraj Singh <dheerajsingh1939@gmail.com>
2026-08-25 12:46:29 +0530

    implement functions to find the last node and calculate the length of a linked list

---
+++ LinkList/length.js
@@ -0,0 +1,15 @@
+// write a program to return the length of the liked list
+
+function length(head) {
+    let curr = head;
+    let count = 0;
+    while (curr != null) {
+
+        count++
+        // fist increase the count 
+        curr = curr.next;
+        // then update the value
+
+    }
+    return count;
+}
